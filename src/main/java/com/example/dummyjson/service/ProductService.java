a972aefd2757b81d0cc50688b58cbc316bcb611f
Renato Novais <renato.novais.silva@hotmail.com>
2024-11-20 19:07:39 -0300

    feat: Migração da camada de service

--- src/main/java/com/example/dummyjson/service/ProductService.java
+++ src/main/java/com/example/dummyjson/service/ProductService.java
@@ -3,28 +3,35 @@ package com.example.dummyjson.service;
 import com.example.dummyjson.dto.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    private final String BASE_URL = "https://dummyjson.com/products";
+    private final WebClient webClient;
 
     @Autowired
-    private RestTemplate restTemplate;
+    public ProductService(WebClient webClient) {
+        this.webClient = webClient;
+    }
 
     public List<Product> getAllProducts() {
-        Product[] products = restTemplate.getForObject(BASE_URL, Product[].class);
+        Product[] products = webClient.get()
+                .uri("/products")
+                .retrieve()
+                .bodyToMono(Product[].class)
+                .block();
         return Arrays.asList(products);
     }
 
     public Product getProductById(Long id) {
-        String url = BASE_URL + "/" + id;
-        return restTemplate.getForObject(url, Product.class);
+        return webClient.get()
+                .uri("/products/{id}", id)
+                .retrieve()
+                .bodyToMono(Product.class)
+                .block();
     }
 }
