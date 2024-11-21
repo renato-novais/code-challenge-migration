2ab6c79b1760868f1555ebec41660560d26b3629
Renato Novais <renato.novais.silva@hotmail.com>
2024-11-21 03:31:37 -0300

    chore/feat: Implementação do Swagger para documentação dos paths das APIs

--- src/main/java/com/example/dummyjson/controller/HealthController.java
+++ src/main/java/com/example/dummyjson/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.example.dummyjson.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+@Tag(name = "Health", description = "Path that returns the health of the microservice")
 @RestController
 @RequestMapping("/health")
 public class HealthController {
 
+    @Operation(summary = "Check health", description = "Get microservice health")
     @GetMapping
     public ResponseEntity<Map<String, Object>> checkHealth() {
         Map<String, Object> healthStatus = new HashMap<>();
--- src/main/java/com/example/dummyjson/controller/ProductController.java
+++ src/main/java/com/example/dummyjson/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.example.dummyjson.controller;
 
 import com.example.dummyjson.dto.Product;
 import com.example.dummyjson.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Tag(name = "Products", description = "Path that interacts with the DummyJSON public API")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
@@ -18,11 +21,13 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Operation(summary = "Get all products", description = "Get all saved products")
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
+    @Operation(summary = "Get product by ID", description = "Get the product by ID code")
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable @NotNull Long id) {
         Product product = productService.getProductById(id);
